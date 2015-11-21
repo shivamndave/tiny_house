9d703f65efe07af5598ed1865f4dae3269553901
Joseph Ou <jou1@ucsc.edu>
2015-11-20 21:32:35 -0800

    added more tests, modified grabCommand() to take uint8_t. figured out the garbled input issue

--- src/firmware/main.c
+++ src/firmware/main.c
@@ -8,7 +8,7 @@
 #include "One_Wire_Library/OneWire.h"
 #include "UART_LIBRARY/uart.h"
 #include <util/delay.h>
-uint16_t *grabCommand(void);
+uint8_t *grabCommand(void);
 ISR(TIM0_OVF_vect);
 
 FSM_t FSM[] = {
@@ -26,14 +26,16 @@ int main (void)
 	//	status->flags ^= EN_BIT;
 		while(true)
 		{
-		if (uart1_available() >= 1) (ProcessCommand());
-		if (status->flags & EN_BIT)
-			{
-			PrintSystemStatusString();
-			FSM[status->currentState].Output_Func_ptr();
-			status->flags = SensorResult();
-			status->currentState = FSM[status->currentState].nextState[status->flags];
-			}
+			//uprintf(RX_TX_FUNCTION_puts, "hello");
+			if (uart1_available() >= 1) (ProcessCommand());
+			if (status->flags & EN_BIT)
+				{
+				PrintSystemStatusString();
+
+				FSM[status->currentState].Output_Func_ptr();
+				status->flags = SensorResult();
+				status->currentState = FSM[status->currentState].nextState[status->flags];
+				}
 		}
 	}
 	FreeMemory();
@@ -41,63 +43,34 @@ int main (void)
 }
 
 
-	uint16_t *grabCommand(void)
+// Takes a byte from the Xbee at uart1 and places it into variable c
+// if a valid delimiter is found, the delimiter is replaced with 0xFF
+// and c is returned.
+	uint8_t *grabCommand(void)
 	{
-		uint16_t *c;
+		uint8_t *c;
+		c = (uint8_t*)malloc(8);
 
-		c = (uint16_t*)malloc(11);
-		c[0] = uart1_getc();
-		if (c[0] == RX_DELIMITER) {
-			c[0] = 0xFF;
-			return c;
+		int temp = 0;
+		for(temp = 0; uart1_available() >= 1; temp++){
+			if (temp == 8)return NULL;
+			/* BYTE TO 2 BYTE FUNCTION
+			if (temp % 2){
+				c[temp] = c[temp] << 8;
+				c[temp] = c[temp] | uart1_getc();
+			}*/
+
+
+			c[temp] = uart1_getc();
+			uprintf(RX_TX_FUNCTION_puts, "gC() = %d - %X\n", temp, c[temp]); //testing
+			if (c[temp] == RX_DELIMITER) {
+				//c[temp] = 0xFF;
+				return c;
 			}
-		c[1] = uart1_getc();
-		if (c[1] == RX_DELIMITER) {
-			c[1] = 0xFF;
-			return c;
-			}
-		c[2] = uart1_getc();
-		if (c[2] == RX_DELIMITER) { 
-			c[2] = 0xFF;
-			return c;
-			}
-		c[3] = uart1_getc();
-		if (c[3] == RX_DELIMITER) {
-			c[3] = 0xFF;
-			return c;
-			}
-		c[4] = uart1_getc();
-		if (c[4] == RX_DELIMITER) {
-			c[4] = 0xFF;
-			return c;
-			}
-		c[5] = uart1_getc();
-		if (c[5] == RX_DELIMITER) {
-			c[5] = 0xFF;
-			return c;
-			}
-		c[6] = uart1_getc();
-		if (c[6] == RX_DELIMITER) {
-			c[6] = 0xFF;
-			return c;
-			}
-		c[7] = uart1_getc();
-		if (c[7] == RX_DELIMITER) {
-			c[7] = 0xFF;
-			return c;
-			}
-		c[8] = uart1_getc();
-		if (c[8] == RX_DELIMITER) {
-			c[8] = 0xFF;
-			return c;
-			}
-		c[9] = uart1_getc();
-		if (c[9] == RX_DELIMITER) {
-			c[9] = 0xFF;
-			return c;
-			}
-			return NULL;
 		}
+		free(c);
+		return NULL;
+	}
 
 // the whole received message is checksummed and stored as a
 // 16-bit word before the null term uint16_t with the _array_checksum algorithm
@@ -111,15 +84,17 @@ int main (void)
 
 uint16_t ProcessCommand(void)
 {
-
-	uint16_t *rxByteArray;
+	uprintf(RX_TX_FUNCTION_puts, "IN PC()\n");
+	uint8_t *rxByteArray;
 	rxByteArray = grabCommand();
 	uart0_putc('C');
-	for (int i = 0; rxByteArray[i] != 0x00FF; i++)
-	{
-		uart0_putc((char)(rxByteArray[i] & 0x00FF));
+	if (rxByteArray != NULL){
+		for (int i = 0; rxByteArray[i] != RX_DELIMITER; i++)
+		{
+			uart0_putc((char)(rxByteArray[i] & 0x00FF));
+			uprintf(RX_TX_FUNCTION_puts, "%c", rxByteArray[i]);
+		}
 	}
-	
 
 	 	/*
 		if (_str_checksum(rxByteArray) != rxByteArray[i - 1])
