06f2a350630d04b0f533b07ac4f323368e1432e5
Sargis Yonan <sargisy@gmail.com>
2016-01-25 17:05:10 -0800

    comment issue while compiling for mega

--- src/firmware/Sensor_Driver/driver.h
+++ src/firmware/Sensor_Driver/driver.h
@@ -72,14 +72,14 @@ bool SystemInit(void);
                  the whole received message is checksummed and stored as a
                  16-bit word before the null term uint16_t with the _array_checksum algorithm
                  the first 16-bits are the command, and the next bytes are the argument to that command
-/*
+*
 / example of packet to send
 /  Rx    = | 0xXX 8-BIT COMMAND | 8-BIT UPPER ARGUMENT | 8-BIT DELIMITER ('-') = 0x002D |
 /  index =                0                                 1                              2                               3
 / THE COMMAND -> Rx = {{RECEIVE_MESSAGE_CHANGE_SETPOINT}, {0x2D}} = changes the set point to 100 degrees Celsius
 */
 
- * ------------
+/* ------------
  * PARAMETERS  : void
  * ------------
  * RETURNS     : void
