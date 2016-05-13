9e379629d9717199cd722fe6c43537c745099b8c
Shivam Dave <shivamndave@gmail.com>
2016-05-13 14:06:57 -0700

    Moved from Commands to OnBoard
    OnBoard currently is a page to enter a message
    and it will be sent out over mqtt. This shows
    that subscription/publishing works because of
    how the messages that are sent over a topic are
    displayed on the screen dynamically:

---
+++ src/MGApp/MGApp/static/onboard/controllers/OnboardController.js
@@ -0,0 +1,44 @@
+var onboardController = angular.module('appController'),
+    CLIENTNAME = "test.mosquitto.org",
+    CLIENTPORT = 8080,
+    CLIENTID   = "id";
+
+onboardController.controller('OnboardController', function($scope) {
+  var client = new Paho.MQTT.Client(CLIENTNAME, CLIENTPORT, CLIENTID);
+
+  console.log("Now trying to connect...");
+  client.connect({onSuccess:onConnect});
+
+  client.onConnectionLost = onConnectionLost;
+  client.onMessageArrived = onMessageArrived;
+
+  $scope.sendMQTTMessage = function(inp) {
+    if(inp) {
+      message = new Paho.MQTT.Message(inp.toString());
+      message.destinationName = "test_thing_we_have";
+      client.send(message);
+    } else {
+      console.log("No String")
+    }
+  }
+
+  // called when the client connects
+  function onConnect() {
+    console.log("onConnect");
+    client.subscribe("test_thing_we_have");
+    console.log("subscribed");
+  }
+
+  // called when the client loses its connection
+  function onConnectionLost(responseObject) {
+    if (responseObject.errorCode !== 0) {
+      console.log("onConnectionLost:"+responseObject.errorMessage);
+    }
+  }
+
+  function onMessageArrived(message) {
+    console.log("onMessageArrived:"+message.payloadString);
+    $scope.mqtt_message = message.payloadString;
+    $scope.$apply();
+  }
+});
