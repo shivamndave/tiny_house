f324da44560c4056de47c801df88c328b0b9f131
Shivam Dave <shivamndave@gmail.com>
2016-05-13 21:54:43 -0700

    [Onboard}
    Added onboard form directive. Will be used to pre populate
    parts of a form for the user based on a mqtt message

--- src/MGApp/MGApp/static/onboard/controllers/OnboardController.js
+++ src/MGApp/MGApp/static/onboard/controllers/OnboardController.js
@@ -1,10 +1,14 @@
 var onboardController = angular.module('appController'),
     CLIENTNAME = "test.mosquitto.org",
     CLIENTPORT = 8080,
-    CLIENTID   = "id";
+    CLIENTID   = "id",
+    NEWSENSORTOPIC="/nodediscover/newsensor";
+
 
 onboardController.controller('OnboardController', function($scope) {
   var client = new Paho.MQTT.Client(CLIENTNAME, CLIENTPORT, CLIENTID);
+  $scope.show_form = false;
+  $scope.sensordata = null;
 
   console.log("Now trying to connect...");
   client.connect({onSuccess:onConnect});
@@ -12,31 +16,46 @@ onboardController.controller('OnboardController', function($scope) {
   client.onConnectionLost = onConnectionLost;
   client.onMessageArrived = onMessageArrived;
 
-  $scope.sendMQTTMessage = function(inp) {
-    if(inp) {
-      message = new Paho.MQTT.Message(inp.toString());
-      message.destinationName = "test_thing_we_have";
-      client.send(message);
-    } else {
-      console.log("No String")
-    }
+  $scope.sendOnboardMessage = function() {
+    message = new Paho.MQTT.Message("Is there a sensor out there");
+    message.destinationName = NEWSENSORTOPIC;
+    client.send(message);
   }
 
   function onConnect() {
     console.log("onConnect");
-    client.subscribe("test_thing_we_have");
+    client.subscribe(NEWSENSORTOPIC);
     console.log("subscribed");
   }
 
   function onConnectionLost(responseObject) {
     if (responseObject.errorCode !== 0) {
-      console.log("onConnectionLost:"+responseObject.errorMessage);
+      console.log("onConnectionLost:" + responseObject.errorMessage);
     }
   }
 
   function onMessageArrived(message) {
-    console.log("onMessageArrived:"+message.payloadString);
-    $scope.mqtt_message = message.payloadString;
+    console.log("onMessageArrived:" + message.payloadString);
+    $scope.show_form = true;
+    $scope.sensordata = {"string": message.payloadString};
     $scope.$apply();
   }
+});
+
+onboardController.directive('onboardForm', function(){
+  return {
+    restrict: 'E',
+    scope: {
+      sensordata: '='
+    },
+    link: link,
+    templateUrl: 'static/onboard/templates/onboard_form.html'
+  }
+    function link(scope,element,attrs) {
+      scope.$watchCollection(attrs.sensordata, function(newValue, oldValue){
+        if(newValue) {
+            console.log(newValue.string);
+            scope.test = newValue.string;
+        }
+      }, true);}
 });
