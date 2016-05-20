2ad0a173fa10eb55dd694448d0e81185d8171849
Shivam Dave <shivamndave@gmail.com>
2016-05-20 02:33:43 -0700

    [Onboard]
    Controller sends start message to the nodediscover, and subscribes
    to another wildcard topic. Based on what is returned as a message and
    utilizing the topic's mac-addr, numbers of sensors is found, parsed, and
    builds a partially filled form.

--- src/MGApp/MGApp/static/onboard/controllers/OnboardController.js
+++ src/MGApp/MGApp/static/onboard/controllers/OnboardController.js
@@ -1,31 +1,47 @@
 var onboardController = angular.module('appController'),
-    CLIENTNAME = "test.mosquitto.org",
-    CLIENTPORT = 8080,
+    CLIENTNAME = "169.233.220.41",
+    CLIENTPORT = 9001,
+    TEST_CLIENTNAME = "test.mosquitto.org",
+    TEST_CLIENTPORT = 8080,
+    TEST_MSG = "DH=2 T=1 DT=0",
+    TEST_MAC = "00:0a:95:9d:68:16"
     CLIENTID   = "id",
-    NEWSENSORTOPIC="/nodediscover/newsensor";
+    TOPIC_INTIALPUB = "testbed/nodeDiscover/command/",
+    TOPIC_INTIALSUB = "testbed/nodeDiscover/data/#",
+    ROOMURL = "api/rooms";
 
 
-onboardController.controller('OnboardController', function($scope) {
-  var client = new Paho.MQTT.Client(CLIENTNAME, CLIENTPORT, CLIENTID);
+onboardController.controller('OnboardController', function($scope, getDataService) {
+  $scope.sensor_data_array = [];
+  var initialClient = new Paho.MQTT.Client(TEST_CLIENTNAME, TEST_CLIENTPORT, CLIENTID);
+  // var initialClient = new Paho.MQTT.Client(CLIENTNAME, CLIENTPORT, CLIENTID);
   $scope.show_form = false;
+  $scope.cancel_button = false;
+  $scope.onConnect_button = true;
   $scope.sensordata = null;
 
   console.log("Now trying to connect...");
-  client.connect({onSuccess:onConnect});
+  initialClient.connect({onSuccess:onConnect});
 
-  client.onConnectionLost = onConnectionLost;
-  client.onMessageArrived = onMessageArrived;
+  initialClient.onConnectionLost = onConnectionLost;
+  initialClient.onMessageArrived = onMessageArrived;
+
+  getDataService(null, ROOMURL).then(function(roomData){
+    console.log(roomData.rooms);
+    $scope.rms = roomData.rooms;
+  });
 
   $scope.sendOnboardMessage = function() {
-    message = new Paho.MQTT.Message("Is there a sensor out there");
-    message.destinationName = NEWSENSORTOPIC;
-    client.send(message);
+    message = new Paho.MQTT.Message("START");
+    message.destinationName = TOPIC_INTIALPUB;
+    initialClient.send(message);
+    initialClient.subscribe(TOPIC_INTIALSUB);
+    console.log("subscribed");
+    $scope.cancel_button = true;
   }
 
   function onConnect() {
-    console.log("onConnect");
-    client.subscribe(NEWSENSORTOPIC);
-    console.log("subscribed");
+    console.log("Successful onConnect");
   }
 
   function onConnectionLost(responseObject) {
@@ -36,9 +52,65 @@ onboardController.controller('OnboardController', function($scope) {
 
   function onMessageArrived(message) {
     console.log("onMessageArrived:" + message.payloadString);
+    var parseDest = message.destinationName.split("/"),
+        mac_address = parseDest[parseDest.length - 1];
+
     $scope.show_form = true;
-    $scope.sensordata = {"string": message.payloadString};
+    // var parsedSensor = parseSensorAdded(message.payloadString, mac_address);
+    var parsedSensor = parseSensorAdded(TEST_MSG, TEST_MAC),
+        sensors_array = formPush(parsedSensor);
+
+    console.log(sensors_array);
+    $scope.sensor_data_array = sensors_array;
     $scope.$apply();
+  }
+
+  function formPush(parsedSensor) {
+    var sensors = [];
+    for(var i = 0; i < parsedSensor.light_sensors; i++) {
+      var iStr = i.toString();
+      sensors.push({"name": "Light Sensor " + iStr, "uid": "l" + iStr, "mac_address": parsedSensor.mac_address, "unit": "Lx", "longunit": "Lux"});
+    }
+    for(var i = 0; i < parsedSensor.humidity_sensors; i++) {
+      var iStr = i.toString();
+      sensors.push({"name": "Humidity Sensor " + iStr, "uid": "h" + iStr, "mac_address": parsedSensor.mac_address, "unit": "RH", "longunit": "Rel. Humidity"});
+    }
+    for(var i = 0; i < parsedSensor.temperature_sensors; i++) {
+      var iStr = i.toString();
+      sensors.push({"name": "Temperature Sensor " + iStr, "uid": "t" + iStr, "mac_address": parsedSensor.mac_address, "unit": "C", "longunit": "Celsius"});
+    }
+    for(var i = 0; i < parsedSensor.humid_temp_sensors; i++) {
+      var iStr = i.toString();
+      sensors.push({"name": "Humidity Temp Sensor " + iStr, "uid": "u" + iStr, "mac_address": parsedSensor.mac_address, "unit": "RH", "longunit": "Rel. Humidity"});
+    }
+
+    return sensors
+
+  }
+
+  function parseSensorAdded(sensorStr, mac_address) {
+    var senssplit = sensorStr.split(" "),
+        sensorInfoDict = {},
+        light_sensors, temp_sensors, hygro_sensors;
+
+    sensorInfoDict['mac_address'] = mac_address;
+
+    senssplit.forEach(function(val) {
+      var tempVal = val.split("=")
+
+
+      console.log(tempVal[0]);
+      switch(tempVal[0]) {
+        case 'L': sensorInfoDict["light_sensors"] = parseInt(tempVal[1]); break;
+        case 'DH': sensorInfoDict["humidity_sensors"] = parseInt(tempVal[1]); break;
+        case 'D': sensorInfoDict["humidity_sensors"] = parseInt(tempVal[1]); break;
+        case 'T': sensorInfoDict["temperature_sensors"] = parseInt(tempVal[1]); break;
+        case 'DT': sensorInfoDict["humid_temp_sensors"] = parseInt(tempVal[1]); break;
+      }
+    });
+
+    console.log(sensorInfoDict);
+    return sensorInfoDict;
   }
 });
 
@@ -46,16 +118,25 @@ onboardController.directive('onboardForm', function(){
   return {
     restrict: 'E',
     scope: {
-      sensordata: '='
+      sensordata: '@',
+      rooms: '@',
+      onSubmit: '&'
     },
     link: link,
     templateUrl: 'static/onboard/templates/onboard_form.html'
   }
     function link(scope,element,attrs) {
-      scope.$watchCollection(attrs.sensordata, function(newValue, oldValue){
-        if(newValue) {
-            console.log(newValue.string);
-            scope.test = newValue.string;
-        }
-      }, true);}
+      scope.$watch(attrs.sensordata, function(data) {
+          scope.mac = data.mac_address;
+          scope.name = data.name;
+          scope.uid = data.uid;
+          scope.unit = data.unit;
+          scope.longunit = data.longunit;
+      });
+
+      scope.$watch(attrs.rooms, function(data) {
+        console.log(data);
+        scope.room_arr = data;
+      });
+    }
 });
