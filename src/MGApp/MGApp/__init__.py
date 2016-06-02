b9380e13c390f7aa2eb872c5064dd92440d4ed65
Shivam Dave <shivamndave@gmail.com>
2016-06-01 21:56:54 -0700

    API
    Actuator queries added. Able to query for all or specific, similar to
    sensors

--- src/MGApp/MGApp/__init__.py
+++ src/MGApp/MGApp/__init__.py
@@ -109,6 +109,92 @@ def sensor_parser(s_item, dict_cursor):
     _dict={"equipment": eq, "sensor_info": s_item, "actuator_info": ai, "values": values_dict}
     return _dict
 
+@app.route("/api/actuators")
+def all_actuators():
+    json = []
+
+    cursor = mysql.connection.cursor()
+    dict_cursor = mysql.connection.cursor(cursorclass=DictCursor)
+
+    dict_cursor.execute('''SELECT * FROM `t_actuator_info`''')
+    ai = dict_cursor.fetchall()
+
+    for at_item in ai:
+        json.append(actuator_parser(at_item, dict_cursor))
+
+    dict_cursor.close()
+    return jsonify(all=json)
+
+@app.route("/api/actuators/<int:a_id>", methods=['GET'])
+def actuator_spec(a_id):
+    #app.logger.info(s_id)
+    json = []
+
+    cursor = mysql.connection.cursor()
+    dict_cursor = mysql.connection.cursor(cursorclass=DictCursor)
+
+    dict_cursor.execute('''SELECT * FROM `t_actuator_info` WHERE id=''' + str(a_id))
+    a_item = dict_cursor.fetchone()
+    if a_item:
+        actuator_object = actuator_parser(a_item, dict_cursor)
+    else:
+        actuator_object = None
+
+    return jsonify(actuator=actuator_object)
+
+def actuator_parser(a_item, dict_cursor):
+    equipment_id = a_item['equipment_id']
+    sensor_id = a_item['sensor_id']
+    actuator_id = a_item['id']
+
+    query_eq = '''SELECT * FROM `t_equipment` WHERE id=''' + str(equipment_id)
+    dict_cursor.execute(query_eq)
+    eq = dict_cursor.fetchone()
+    # app.logger.info(query_eq)
+    # app.logger.info(eq)
+
+    query_si = '''SELECT * FROM `t_sensor_info` WHERE id=''' + str(sensor_id)
+    dict_cursor.execute(query_si)
+    si = dict_cursor.fetchall()
+    # app.logger.info(query_ai)
+    # app.logger.info(ai)
+
+
+    values_si = []
+    if si:
+        query_data_si = '''SELECT * FROM `t_data` WHERE sensor_id=''' + str(sensor_id) + ''' ORDER BY timestamp ASC'''
+        dict_cursor.execute(query_data_si)
+
+        for val in dict_cursor.fetchall():
+            values_si.append([int(val['timestamp'].strftime("%s"))*1000, val['value']])
+
+    values_ai = []
+    query_data_ai = '''SELECT * FROM `t_data` WHERE actuator_id=''' + str(actuator_id) + ''' ORDER BY timestamp ASC'''
+    dict_cursor.execute(query_data_ai)
+    # app.logger.info(query_data_si)
+
+    for val in dict_cursor.fetchall():
+        values_ai.append([int(val['timestamp'].strftime("%s"))*1000, val['value']])
+
+    # app.logger.info(values_si)
+    values_dict = {"sensor": values_si, "actuator": values_ai}
+
+    query_data_status = '''SELECT * FROM `t_data` WHERE sensor_id=''' + str(sensor_id) + ''' ORDER BY timestamp DESC LIMIT 0, 1'''
+    dict_cursor.execute(query_data_status)
+    status = dict_cursor.fetchone()
+    app.logger.info(status)
+    status_dict = {}
+    if status is None or status['value'] == 9999:
+        status_dict = {"status": 0}
+    else:
+        status_dict = {"status": 1}
+
+    for si_item in si:
+        si_item.update(status_dict)
+
+    _dict={"equipment": eq, "sensor_info": si, "actuator_info": a_item, "values": values_dict}
+    return _dict
+
 @app.route("/api/rooms")
 def rooms():
     cursor = mysql.connection.cursor()
