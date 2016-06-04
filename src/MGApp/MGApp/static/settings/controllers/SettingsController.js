24f2d5333a472f3b264c08f65909ee6761052ecd
Shivam Dave <shivamndave@gmail.com>
2016-06-03 19:24:26 -0700

    Settings
    Passes necessay data to settings table to allow navigation to edit pages

--- src/MGApp/MGApp/static/settings/controllers/SettingsController.js
+++ src/MGApp/MGApp/static/settings/controllers/SettingsController.js
@@ -11,31 +11,48 @@ settingsController.controller('SettingsController', function($scope, getDataServ
     $scope.data_value = data.rooms;
   });
 
+  $scope.idGet = function(value, obj) {
+    console.log(obj);
+      switch(value) {
+        case "sensors":
+          if (obj.sensor_info) {
+            return obj.sensor_info;
+          }
+        case "rooms":
+          return obj;
+        case "actuators":
+          if (obj.actuator_info) {
+            return obj.actuator_info;
+          }
+        case "equipment":
+          return obj;
+      }
+  }
   // Changes based on radio button selection
   $scope.newSettingView = function(value) {
     switch(value) {
       case "sensors":
         console.log("We chose: "+ value);
-        getDataService(null, SENSORSURL).then(function(data) {
-          $scope.data_value = data.all;
+        getDataService(null, SENSORSURL).then(function(sensorData) {
+          $scope.data_value = sensorData.all;
         });
         break;
       case "rooms":
         console.log("We chose: "+ value);
-        getDataService(null, ROOMSURL).then(function(data) {
-          $scope.data_value = data.rooms;
+        getDataService(null, ROOMSURL).then(function(roomData) {
+          $scope.data_value = roomData.rooms;
         });
         break;
       case "actuators":
         console.log("We chose: "+ value);
-        getDataService(null, ACTUATORSURL).then(function(data) {
-          $scope.data_value = data.all;
+        getDataService(null, ACTUATORSURL).then(function(actuatorData) {
+          $scope.data_value = actuatorData.all;
         });
         break;
       case "equipment":
         console.log("We chose: "+ value);
-        getDataService(null, EQUIPMENTURL).then(function(data) {
-          $scope.data_value = data.equipment;
+        getDataService(null, EQUIPMENTURL).then(function(equipmentData) {
+          $scope.data_value = equipmentData.equipment;
         });
         break;
     }
