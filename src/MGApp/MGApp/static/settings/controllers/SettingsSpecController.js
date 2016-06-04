add448d14aa502212566527199dd0a87558cc0de
Shivam Dave <shivamndave@gmail.com>
2016-06-03 19:35:56 -0700

    Settings
    Post data pass to API call for each settings sepcific page.

--- src/MGApp/MGApp/static/settings/controllers/SettingsSpecController.js
+++ src/MGApp/MGApp/static/settings/controllers/SettingsSpecController.js
@@ -7,7 +7,7 @@ var settingsRoomsController = angular.module('appController'),
     ACTUATORSURL = "api/actuators",
     EQUIPMENTURL = "api/equipment";
 
-settingsRoomsController.controller('SettingsRoomsController', function($scope, $routeParams, getDataService) {
+settingsRoomsController.controller('SettingsRoomsController', function($scope, $routeParams, getDataService, postToDB) {
     getDataService($routeParams.id, ROOMSURL).then(function(data) {
         currentData = data.room;
         console.log(currentData);
@@ -16,9 +16,24 @@ settingsRoomsController.controller('SettingsRoomsController', function($scope, $
         $scope.fieldrtype = currentData.type;
         $scope.fieldrinfo = currentData.info;
     });
+
+    $scope.room_edit = function(fieldrid, fieldrname, fieldrtype, fieldrinfo) {
+        console.log(fieldrid + ", " + fieldrname + ", " + fieldrtype + ", " + fieldrinfo)
+        var postData = {
+          "name": fieldrname,
+          "type": fieldrtype,
+          "info": fieldrinfo,
+          "id": parseInt(fieldrid),
+        };
+        postToDB("/api/rooms/update", postData).then(function(value) {
+          console.log("Success - Button disabled")
+        }, function(reason) {
+          console.log("Rejected SAHN")
+        });
+    }
 });
 
-settingsEquipmentController.controller('SettingsEquipmentController', function($scope, $routeParams, getDataService) {
+settingsEquipmentController.controller('SettingsEquipmentController', function($scope, $routeParams, getDataService, postToDB) {
     getDataService($routeParams.id, EQUIPMENTURL).then(function(data) {
         currentData = data.equipment;
         console.log(currentData);
@@ -34,10 +49,22 @@ settingsEquipmentController.controller('SettingsEquipmentController', function($
 
     $scope.equipment_edit = function(fieldeid, fieldename, fieldeinfo, fieldelocation, fieldmacaddress, fieldroomselected) {
         console.log(fieldeid + ", " + fieldename + ", " + fieldeinfo + ", " + fieldelocation + ", " + fieldmacaddress + ", " + fieldroomselected)
+        var postData = {
+            "room_id": fieldroomselected,
+            "name": fieldename,
+            "location": fieldelocation,
+            "info": fieldeinfo,
+            "id": parseInt(fieldeid),
+        };
+        postToDB("/api/equipment/update", postData).then(function(value) {
+          console.log("Success - Button disabled")
+        }, function(reason) {
+          console.log("Rejected SAHN")
+        });
     }
 });
 
-settingsSensorsController.controller('SettingsSensorsController', function($scope, $routeParams, getDataService) {
+settingsSensorsController.controller('SettingsSensorsController', function($scope, $routeParams, getDataService, postToDB) {
     getDataService($routeParams.id, SENSORSURL).then(function(data) {
         currentData = data.sensor.sensor_info;
         console.log(currentData);
@@ -47,32 +74,50 @@ settingsSensorsController.controller('SettingsSensorsController', function($scop
         $scope.fieldsinfo = currentData.info;
         $scope.fieldsunit = currentData.unit;
         $scope.fieldslongunit = currentData.longunit;
-        $scope.fieldequipmentselected = currentData.equipment_id;
-    }).then(getDataService(null, EQUIPMENTURL).then(function(equipmentdata) {
-        $scope.fieldequipment = equipmentdata.equipment;
-    }));
+        $scope.fieldequipment = data.sensor.equipment.name;
+    })
 
-    $scope.sensor_edit = function(fieldsid, fieldsuid, fieldsname, fieldsinfo, fieldsunit, fieldslongunit, fieldequipmentselected) {
-        console.log(fieldsid + ", " +fieldsuid + ", " +fieldsname + ", " +fieldsinfo + ", " +fieldsunit + ", " +fieldslongunit + ", " +fieldequipmentselected)
+    $scope.sensor_edit = function(fieldsid, fieldsuid, fieldsname, fieldsinfo, fieldsunit, fieldslongunit, fieldequipment) {
+        console.log(fieldsid + ", " +fieldsuid + ", " +fieldsname + ", " +fieldsinfo + ", " +fieldsunit + ", " +fieldslongunit + ", " +fieldequipment)
+        var postData = {
+            "name": fieldsname,
+            "unit": fieldsunit,
+            "longunit": fieldslongunit,
+            "info": fieldsinfo,
+            "id": parseInt(fieldsid),
+        };
+        postToDB("/api/sensors/update", postData).then(function(value) {
+          console.log("Success - Button disabled")
+        }, function(reason) {
+          console.log("Rejected SAHN")
+        });
     }
 });
 
-settingsActuatorsController.controller('SettingsActuatorsController', function($scope, $routeParams, getDataService) {
+settingsActuatorsController.controller('SettingsActuatorsController', function($scope, $routeParams, getDataService, postToDB) {
     getDataService($routeParams.id, ACTUATORSURL).then(function(data) {
         currentData = data.actuator.actuator_info;
         console.log(currentData);
         $scope.fieldaid = currentData.id;
         $scope.fieldaname = currentData.name;
         $scope.fieldainfo = currentData.info;
-        $scope.fieldsensorselected = currentData.sensor_id;
-        $scope.fieldequipmentselected = currentData.equipment_id;
-    }).then(getDataService(null, SENSORSURL).then(function(sensordata) {
-        $scope.fieldsensors = sensordata.all;
-    })).then(getDataService(null, EQUIPMENTURL).then(function(equipmentdata) {
-        $scope.fieldequipment = equipmentdata.equipment;
-    }));
+        $scope.fieldsensor = data.actuator.sensor_info.name;
+        $scope.fieldequipment = data.actuator.equipment.name;
+    })
 
-    $scope.actuator_edit = function(fieldaid, fieldaname, fieldainfo, fieldequipmentselected, fieldsensorselected) {
-        console.log(fieldaid + ", " + fieldaname + ", " +  fieldainfo + ", " + fieldequipmentselected + ", " + fieldsensorselected)
+    $scope.actuator_edit = function(fieldaid, fieldaname, fieldainfo, fieldequipment, fieldsensor) {
+        console.log(fieldaid + ", " + fieldaname + ", " +  fieldainfo + ", " + fieldequipment + ", " + fieldsensor)
+
+        var postData = {
+            "name": fieldaname,
+            "info": fieldainfo,
+            "id": parseInt(fieldaid),
+        };
+
+        postToDB("/api/actuators/update", postData).then(function(value) {
+          console.log("Success - Button disabled")
+        }, function(reason) {
+          console.log("Rejected SAHN")
+        });
     }
 });
